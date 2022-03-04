45a21f8e98c5116b90267279fce66bf1cecdca76
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-03-04 11:45:58 -0700

    added overview functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
 var movieElement = document.querySelector("#movies");
 
 // fetch movies based on genre and streaming service
-var streamingAPI = function(streamingService,genre) {
+var streamingAPI = function (streamingService, genre) {
     fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=" + streamingService + "&type=movie&genre=" + genre + "&page=1&output_language=en&language=en", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "streaming-availability.p.rapidapi.com",
-		"x-rapidapi-key": "454448608emsh73079d61e5d57cep1bb207jsn7a7395c1a6e0"
-	}
-})
-.then(response => {
-	
-    if (response.ok) {
-        response.json().then(function(data){
-            // send data to displaystreaming function to display on page
-            displayStreaming(data);
-            // send movie titles to moviedb api
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+            "x-rapidapi-key": "454448608emsh73079d61e5d57cep1bb207jsn7a7395c1a6e0"
+        }
+    })
+        .then(response => {
 
+            if (response.ok) {
+                response.json().then(function (data) {
+                    // send data to displaystreaming function to display on page
+                    displayStreaming(data);
+                    // send movie titles to moviedb api
+
+                })
+            }
         })
-    }
-})
-.catch(err => {
-	console.error(err);
-});
+        .catch(err => {
+            console.error(err);
+        });
 };
 
 
@@ -30,13 +30,18 @@ var displayStreaming = function (movies) {
     console.log(movies);
 
     // iterate through movie data
-    for (var i = 0; i < 3; i++){
-        
+    for (var i = 0; i < 3; i++) {
+
         //display movie name
-        var nameEl = movieElement.querySelector("#movieName-"+ i);
+        var nameEl = movieElement.querySelector("#movieName-" + i);
         var movieName = movies.results[i].title;
         nameEl.textContent = movieName;
         console.log(movieName);
+
+        // display movie overview
+        var overviewEl = movieElement.querySelector("#overview-"+ i);
+        var movieOverview = movies.results[i].overview;
+        overviewEl.textContent = movieOverview;
 
         // display movie image
         var image = movies.results[i].posterURLs[92];
