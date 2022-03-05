f00321cf71717a26580cb4aa789a6481329e04e8
Eric Shumway <ericshumway2@mail.weber.edu>
2022-03-05 13:48:51 -0700

    Edits to recieve information from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,11 @@ const netflix = document.querySelector("#netflix");
 const hulu = document.querySelector("#hulu");
 
 
+
 // fetch movies based on genre and streaming service
 var streamingAPI = function (streamingService, genre) {
 
-    // streamingService = whatever streaming service checkbox is checked
+    /* streamingService = whatever streaming service checkbox is checked
     var streamingService = function () {
         if (disney.checked) {
             streamingService = disney;
@@ -19,7 +20,7 @@ var streamingAPI = function (streamingService, genre) {
         else {
             streamingService = hulu;
         };
-    };
+    };*/
 
     fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=" + streamingService + "&type=movie&genre=" + genre + "&page=1&output_language=en&language=en", {
         "method": "GET",
@@ -49,24 +50,70 @@ var displayStreaming = function (movies) {
     console.log(movies);
 
     // iterate through movie data
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < movies.results.length; i++) {
+        var entryIndex = i;
 
-        //display movie name
-        var nameEl = movieElement.querySelector("#movieName-" + i);
-        var movieName = movies.results[i].title;
-        nameEl.textContent = movieName;
-        console.log(movieName);
+        fetch("https://api.themoviedb.org/3/search/movie?api_key=aa604aaef9964db123112323ebc2e25b&query=" + movies.results[i].title.replace(" ", "+"))
+            .then(response => {
 
-        // display movie overview
-        var overviewEl = movieElement.querySelector("#overview-" + i);
-        var movieOverview = movies.results[i].overview;
-        overviewEl.textContent = movieOverview;
+                if (response.ok) {
+                    response.json().then(function (data) {
+                        var templet = `
+                        <hr>
+                        <h4 >${data.results[0].title}</h4>
+                        <hr>
+                        <p class="rating">&starf;&starf;&starf;&starf;&starf;</p>
+                        <p >${data.results[0].overview}</p>
+                        <!-- pull movie image from API -->
+                        <img alt="">
+                        <div class="streaming-services columns is-mobile is-centered">
+                            <!-- add image from API for streaming services -->
+                            <button class="button is-info is-medium is-outlined column is-3">Watch on Disney+</button>
+                            <button class="button is-danger is-medium is-outlined column is-3">Watch on Netflix</button>
+                            <button class="button is-success is-medium is-outlined column is-3">Watch on Hulu</button>
+                        </div>`
+                        var resultsDiv = document.createElement("div");
+                        resultsDiv.innerHTML = templet;
+                        movieElement.append(resultsDiv);
+                        debugger;
+                        /*/ send data to displaystreaming function to display on page
+                        //display movie name
+                        var nameEl = document.getElementById("movieName-" + entryIndex);
+                        console.log(nameEl);
+                        var movieName = data.results[0].title;
+                        console.log(movieName);
+                        nameEl.textContent = movieName;
 
-        // display movie image
-        var image = movies.results[i].posterURLs[92];
-        var imageEl = movieElement.querySelector("#movieImage-" + i);
-        imageEl.setAttribute("src", image);
-        imageEl.setAttribute("alt", "Movie poster");
+
+
+                        // display movie overview
+                        var overviewEl = document.querySelector("#overview-" + entryIndex);
+                        var movieOverview = data.results[0].overview;
+                        overviewEl.textContent = movieOverview;
+
+                        //display movie image
+                        var image = data.results[0].posterURLs[i];
+                        var imageEl = document.querySelector("#movieImage-" + entryIndex);
+                        imageEl.setAttribute("src", image);
+                        imageEl.setAttribute("alt", "Movie poster");*/
+
+
+
+
+
+                    })
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+
     }
 };
+
+
+streamingAPI("netflix", "1");
+
+
+
 
