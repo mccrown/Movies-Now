cdc6c9c54220a4ebc33baaeaba15b64234e49f7b
CMcCown <connermccown94@gmail.com>
2022-03-11 10:15:18 -0700

    loaded link history from localstorage and displayed on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ var disneyBtn = document.querySelector(".disney-btn");
 var netflixBtn = document.querySelector(".netflix-btn");
 var huluBtn = document.querySelector(".hulu-btn");
 var linkHistory = JSON.parse(localStorage.getItem("links")) || [];
+var historyEl = document.getElementById("history");
 
 
 
@@ -105,16 +106,19 @@ var displayButton = function (streamingService, data) {
         //console.log(streamingService);
         if (streamingService === "disney") {
             link = data.results[i].streamingInfo.disney.us.link;
-            streamingDisBtn(link, index);
+            title = data.results[i].title;
+            streamingDisBtn(link, index, title);
         }
         else if (streamingService === "netflix") {
             link = data.results[i].streamingInfo.netflix.us.link;
-            streamingNetBtn(link, index);
+            title = data.results[i].title;
+            streamingNetBtn(link, index, title);
 
         }
         else {
             link = data.results[i].streamingInfo.hulu.us.link;
-            streamingHuluBtn(link, index);
+            title = data.results[i].title;
+            streamingHuluBtn(link, index, title);
         }
         index++;
 
@@ -124,29 +128,41 @@ var displayButton = function (streamingService, data) {
     }
 };
 
-var streamingNetBtn = function (link, index) {
+var streamingNetBtn = function (link, index, title) {
     for (i = 0; i < 5; i++)
+    var linkSave = {
+        clickedLink: link,
+        title: title
+    }
         document.getElementById("netflix-btn-" + index).onclick = function () {
             location.href = link;
-            linkHistory.push(link);
+            linkHistory.push(linkSave);
             localStorage.setItem("links", JSON.stringify(linkHistory));
         }
 };
 
-var streamingDisBtn = function (link, index) {
+var streamingDisBtn = function (link, index, title) {
     for (i = 0; i < 5; i++)
+        var linkSave = {
+            clickedLink: link,
+            title: title
+        }
         document.getElementById("disney-btn-" + index).onclick = function () {
             location.href = link;
-            linkHistory.push(link);
+            linkHistory.push(linkSave);
             localStorage.setItem("links", JSON.stringify(linkHistory));
         }
 };
 
-var streamingHuluBtn = function (link, index) {
+var streamingHuluBtn = function (link, index, title) {
     for (i = 0; i < 5; i++)
+    var linkSave = {
+        clickedLink: link,
+        title: title
+    }
         document.getElementById("hulu-btn-" + index).onclick = function () {
             location.href = link;
-            linkHistory.push(link);
+            linkHistory.push(linkSave);
             localStorage.setItem("links", JSON.stringify(linkHistory));
         }
 };
@@ -158,7 +174,23 @@ var loadLinkHistory = function () {
     createHistoryEl(searchHis);
 }
 
-
+var createHistoryEl = function (searchHis) {
+    for (var i = 0; i < searchHis.length; i++) {
+        const link = searchHis[i].clickedLink
+        const title = searchHis[i].title;
+        var historyItem = document.createElement("input");
+        historyItem.setAttribute("type", "text");
+        historyItem.setAttribute("readonly", true);
+        historyItem.setAttribute("clas", "is-link is-light is-centered is-normal is-fullwidth")
+        historyItem.setAttribute("href", link);
+        historyItem.setAttribute("value", title);
+        historyItem.addEventListener("click", function (event){
+            event.preventDefault();
+            location.href = link;
+        })
+        historyEl.append(historyItem);
+    }
+}
 
 searchButton.addEventListener("click", function () {
     var streaming;
