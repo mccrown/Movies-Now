622928199af739e4331290297984c4a6ec7ceeaa
CMcCown <connermccown94@gmail.com>
2022-03-10 18:48:37 -0700

    fixed image issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,8 @@ var displayStreaming = function (movies) {
         var imageEl = movieElement.querySelector("#movieImage-" + i);
         imageEl.setAttribute("src", image);
         imageEl.setAttribute("alt", "Movie poster");
-        imageEl.width = "100px";
-        imageEl.height = "200px";
+        image.width = "100px";
+        image.height = "200px";
 
 
         var index = 0;
